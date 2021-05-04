ed39555659d28b2bf474b9b8ca620b0a5366ff91
Eston Kagwima <ekagwima745@gmail.com>
2021-05-04 16:56:35 +0300

    created user model and get by i method (#4)

--- app/auth/v1/__init__.py
+++ app/auth/v1/__init__.py
@@ -1,6 +1,15 @@
 from flask import Blueprint
 from flask_restful import Api
 
-version1 = Blueprint('auth_v1', __name__, url_prefix='/api/v1')
 
-app = Api(version1, catch_all_404s=True)
+from app.auth.v1.views.user_views import Users
+
+version1 = Blueprint('auth_v1', __name__)
+
+
+api = Api(version1, catch_all_404s=True)
+
+api.add_resource(Users, '/auth/users', strict_slashes=False)
+
+
+
---
+++ app/auth/v1/views/user_views.py
@@ -0,0 +1,11 @@
+from flask import jsonify,make_response,request
+from flask_restful import Resource
+
+from app.auth.v1.model.user_model import UserModels
+
+class Users(Resource):
+  '''Class to register user(s)'''
+
+  def get(self):
+    #Default to 200 ok
+    return { 'hello': 'world'}
