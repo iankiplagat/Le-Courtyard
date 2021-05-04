524db2d3b69974e1e04a582a3acaf77a0007c2a2
Kasparov <ianjkiplagat@gmail.com>
2021-05-04 11:10:02 +0300

    register v1 blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,8 @@ def create_app(config_name):
   app = Flask(__name__, instance_relative_config=True)
   
   app.config.from_object(app_config[config_name])
-  ##############
+  app.config.from_pyfile('config.py')
   app.url_map.strict_slashes = False
-  
+  app.register_blueprint(v1)
   
   return app
--- app/auth/v1/__init__.py
+++ app/auth/v1/__init__.py
@@ -3,4 +3,4 @@ from flask_restful import Api
 
 version1 = Blueprint('auth_v1', __name__, url_prefix='/api/v1')
 
-app = Api(version1, catch_all_404s = True)
+app = Api(version1, catch_all_404s=True)
