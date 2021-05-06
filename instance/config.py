77e5cabf03870fe78ee89435e73476fb36fb5066
Kasparov <ianjkiplagat@gmail.com>
2021-05-06 16:48:37 +0300

    merge

--- instance/config.py
+++ instance/config.py
@@ -5,6 +5,7 @@ class Config(object):
 
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     CSRF_ENABLED = True
 
 
